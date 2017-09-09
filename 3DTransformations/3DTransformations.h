a58a1e2277cac9f474b031e9d33fc229486603d1
Alex Nemkovich <alexey3nemckovich@gmail.com>
2017-09-09 21:45:30 +0300

    Initial commit.

---
+++ 3DTransformations/3DTransformations.h
@@ -0,0 +1,35 @@
+
+// 3DTransformations.h : main header file for the 3DTransformations application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CMy3DTransformationsApp:
+// See 3DTransformations.cpp for the implementation of this class
+//
+
+class CMy3DTransformationsApp : public CWinApp
+{
+public:
+	CMy3DTransformationsApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy3DTransformationsApp theApp;
